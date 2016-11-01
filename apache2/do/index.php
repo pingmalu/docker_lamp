589bbe00e9e8cd1596dc28f817008bacaa7fa495
pingmalu <malu@malu.me>
2016-11-01 18:01:22 +0800

    do

--- apache2/do/index.php
+++ apache2/do/index.php
@@ -1,6 +1,7 @@
 <a href="?type=1">start</a>
 <a href="?type=2">stop</a>
 <a href="?type=3">PS</a>
+<a href="/webssh">webssh</a>
 <pre>
 <?php
 switch($_GET['type']){
@@ -11,7 +12,7 @@ switch($_GET['type']){
     system('../usr /stopgateone.sh');
     break;
   case 3:
-    system('../usr ps -ef');
+    system('../usr ps -e -o \'user,pid,ppid,stime,pcpu,rsz,vsz,args\'');
     break;
 }
 ?>
