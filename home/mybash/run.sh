3ea0c63f63b865725fb32cace1ca441ba71cfd63
pingmalu <malu@malu.me>
2017-02-20 17:48:46 +0800

    svn

--- home/mybash/run.sh
+++ home/mybash/run.sh
@@ -2,6 +2,7 @@
 #MYENV=malu
 if [ "${MYENV}" != "**None**" ] && [ "${MYENV}" != "" ]; then
    cd /app/mybash/${MYENV}
+   ln -s -f -n /app/mybash/${MYENV} /root/myenv
    sed -i 's/.*AllowOverride FileInfo.*/                AllowOverride All/' /etc/apache2/sites-enabled/000-default.conf
    sed -i 's/^files = .*/files = \/app\/supervisor_conf\/*.conf \/app\/mybash\/'${MYENV}'\/*.conf/' /etc/supervisor/supervisord.conf
    #sed -i 's/^files = .*/files = \/app\/mybash\/'${MYENV}'\/*.conf/' /etc/supervisor/supervisord.conf
@@ -13,6 +14,8 @@ if [ "${MYENV}" != "**None**" ] && [ "${MYENV}" != "" ]; then
    #chmod 777 /app/mysql -R
    #PORT proxy
    #ln -s -f /app/mybash/${MYENV}/rinetd.cnf /etc/rinetd.conf
+   #ln -s -f -n /app/mybash/${MYENV}/www /var/www/html
+   #cp -r /app/mybash/.mysec/.subversion /root/
 fi
 #/app/proxy-mysql/mysql-proxy/bin/mysql-proxy --defaults-file=/app/proxy-mysql/mysql-proxy/mysql-proxy.conf &
 #sed -i 's/^#server-id.*/server-id               = 1/' /etc/mysql/my.cnf
